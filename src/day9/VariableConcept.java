8823aac37da1218927cbb6bd6518eaf0281374b6
Ashish Mishra <ashish.mishra.sse@gmail.com>
2025-01-23 21:43:47 +0530

    PrimitiveDataTypeConcept

---
+++ src/day9/VariableConcept.java
@@ -0,0 +1,26 @@
+package day9;
+
+public class VariableConcept {
+    public static void main(String[] args) {
+
+        //1. Variables
+        //a) dataType variableName = value;
+        //byte [-128 to 127][1 Byte = 8 bits]
+        byte x = 127;
+        //short [-32768 to 32767] [2 byte = 16 bits]
+        short y = 32767;
+        //int [-2147483648 to 2147483647][4 byte = 32 bits]
+        int z = -2147483648;
+        //long[-9223372036854775808 to 9223372036854775807][8 byte = 64 bits]
+        long a = 2147483648l;
+        // WAP to print sum of two numbers -12, 8
+        byte b = 30;
+        byte c = 120;
+        //byte sum =b+c; We should never store anything in byte if it involves mathematical calculations
+
+
+    }
+}
+
+
+
