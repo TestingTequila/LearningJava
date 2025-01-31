12e374980af5ff27316088ef58b4a6ba6242f2ae
Ashish Mishra <ashish.mishra.sse@gmail.com>
2025-01-31 20:16:33 +0530

    Loops: While, for loop

---
+++ src/day14/ForLoopConcept.java
@@ -0,0 +1,80 @@
+package day14;
+
+public class ForLoopConcept
+{
+    public static void main(String[] args)
+    {
+        // Print the number from 1-10; 1,2,3,4,5,6,7,8,9,10
+        //for loop
+
+//        for (int i=1;i<=10;i++)
+//        {
+//            System.out.println(i); //1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+//        }
+
+        //Print numbers from 10 to 1
+
+//        for (int i =10;i>=1;i--)
+//        {
+//            System.out.println(i); //10, 9, 8,7,6,5,4,3,2, 1
+//
+//        }
+
+//
+//        for (;true;)
+//        {
+//            System.out.print("Welcome to Hilton Hotel");
+//        }
+
+        //table of 2: 2,4,8,10,12,14,16,18,20
+
+//
+//        for (int i=1;i<=10;i++)
+//        {
+//            System.out.println(2*i); //2, 4,  6, 8, 10, 12,
+//            //6
+//        }
+
+        // Print number from 1-10, except 6
+
+//
+//        for (int i=1;i<=10;i++)
+//        {
+//            if(!(i==6))
+//            {
+//                System.out.println(i);
+//            }
+//
+//        }
+
+        //1-50
+
+//
+//        for (int i=1;i<=50;i++)
+//        {
+//            if(!(i%5==0))
+//            {
+//                System.out.println(i);
+//            }
+//
+//        }
+
+
+        for (int i=1;i<=50;i++)
+        {
+            if(!(i==33))
+            {
+                System.out.println(i);
+
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+}
+
+
+//1,2,3,4,5,6,7,8,9,11,12,13,14,15,33,16,17,18,19,20
+
---
+++ src/day14/WhileLoopConcept.java
@@ -0,0 +1,82 @@
+package day14;
+
+public class WhileLoopConcept
+{
+    public static void main(String[] args)
+    {
+        // Print the number from 1-10; 1,2,3,4,5,6,7,8,9,10
+        //while loop
+//        int i=1;
+//        while (i<=10)
+//        {
+//            System.out.println(i); //1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+//            i++;
+//
+//        }
+
+        //Print numbers from 10 to 1
+//        int i =10;
+//        while (i>=1)
+//        {
+//            System.out.println(i); //10, 9, 8,7,6,5,4,3,2, 1
+//            i--;
+//        }
+
+//
+//        while (true)
+//        {
+//            System.out.print("Welcome to Hilton Hotel");
+//        }
+
+        //table of 2: 2,4,8,10,12,14,16,18,20
+
+//        int i=1;
+//        while (i<=10)
+//        {
+//            System.out.println(2*i); //2, 4,  6, 8, 10, 12,
+//            i++;//6
+//        }
+
+        // Print number from 1-10, except 6
+//        int i=1;
+//
+//        while (i<=10)
+//        {
+//            if(!(i==6))
+//            {
+//                System.out.println(i);
+//            }
+//            i++;
+//        }
+
+        //1-50
+//        int i=1;
+//
+//        while (i<=50)
+//        {
+//            if(!(i%5==0))
+//            {
+//                System.out.println(i);
+//            }
+//            i++;
+//        }
+        int i=1;
+
+        while (i<=50)
+        {
+            if(!(i==33))
+            {
+                System.out.println(i);
+                i++;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+}
+
+
+//1,2,3,4,5,6,7,8,9,11,12,13,14,15,33,16,17,18,19,20
+
